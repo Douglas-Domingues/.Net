1ac85a8d43c3cb30ff2191682e0614d5454c4234
Douglas <domingues.douglas95@gmail.com>
2022-11-21 11:51:59 -0300

    Adição de cursos e interações com listas de alunos. Criação de propriedades datetime.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
 ﻿using Models;
+using System.Globalization; //System.Globalization usado para alterar a região padrão aplicada ao sistema, alterando formatações.
 
-Pessoa p1 = new Pessoa();
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR"); //Definição de qual será a região aplicada em toda a solução.
 
-p1.Nome = "Douglas";
-p1.Idade = 27;
+Pessoa p1 = new Pessoa(nome: "Douglas", sobrenome: "Domingues", datainc: DateTime.Now);
+Pessoa p2 = new Pessoa(nome: "Isabela", sobrenome: "Domingues", datainc: DateTime.Now);
+Pessoa p3 = new Pessoa(nome: "Isadora", sobrenome: "Braga", datainc: DateTime.Now);
 
-p1.Apresentar();
+Curso c1 = new Curso(nome: "Curso de Canto", valor: 1299.99M);
+c1.Alunos = new List<Pessoa>();
 
+c1.AdicionaAluno(p1);
+c1.AdicionaAluno(p2);
+c1.AdicionaAluno(p3);
+
+Console.WriteLine($"O Curso {c1.Nome} possui {c1.ContagemAlunos()} Alunos");
+c1.ListaAlunos();
