e8b6d86c2b3163fd63dc2929eac4886dc49dd43c
Douglas <domingues.douglas95@gmail.com>
2022-11-23 10:19:41 -0300

    Envio Atualizado Corrigido(Em teoria).

--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using System.Globalization; //System.Globalization usado para alterar a região padrão aplicada ao sistema, alterando formatações.
 using System.Xml.Linq;
 // --------------------- Tuplas permitem criar uma variável com vários dados de tipos diferentes, semelhante a um objeto, mas sua declaração é mais simples.
-(int, string, string) exemplotupla = (1, "Douglas", "Domingues");
 
-Console.WriteLine(exemplotupla);
+  (int, string, string) exemplotupla = (1, "Douglas", "Domingues");
+
+  Console.WriteLine(exemplotupla);
+ 
 
 
 // --------------------- Dictionary permite definir uma chave e valor vinculado à chave
@@ -60,30 +62,42 @@ foreach (int elemento in pilha)
 
 // --------------------- Filas possuem uma lógica sequencial, sempre trabalhando com os elementos na ordem que foram declarados. famoso 'FIFO' (first in first out)
 /*
-Queue<int> fila = new Queue<int>();
+Queue<int> fila = new Queue<int>(); // declaração
 
-fila.Enqueue(1);
+fila.Enqueue(1); //insere valor na fila
 fila.Enqueue(8);
 fila.Enqueue(6);
 fila.Enqueue(4);
 
-foreach (int i in fila)
+foreach (int valor in fila)
 {
-    Console.WriteLine(i);
+    Console.WriteLine(valor);
 }
 
+fila.Dequeue(); //Remove o primeiro valor da fila.
+fila.Enqueue(fila.Dequeue()); //joga o primeiro valor da fila para última posição.
+
+foreach (int valor2 in fila)
+{
+    Console.WriteLine(valor2);
+}
+*/
+
+// --------------------- Usando as propriedades, métodos e construtores
+/*
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR"); //Definição de qual será a região aplicada em toda a solução.
 
-//Pessoa p1 = new Pessoa(nome: "Douglas", sobrenome: "Domingues", datainc: DateTime.Now);
-//Pessoa p2 = new Pessoa(nome: "Isabela", sobrenome: "Domingues", datainc: DateTime.Now);
-//Pessoa p3 = new Pessoa(nome: "Isadora", sobrenome: "Braga", datainc: DateTime.Now);
+Pessoa p1 = new Pessoa(nome: "Douglas", sobrenome: "Domingues", datainc: DateTime.Now);
+Pessoa p2 = new Pessoa(nome: "Isabela", sobrenome: "Domingues", datainc: DateTime.Now);
+Pessoa p3 = new Pessoa(nome: "Isadora", sobrenome: "Braga", datainc: DateTime.Now);
 
-//Curso c1 = new Curso(nome: "Curso de Canto", valor: 1299.99M);
-//c1.Alunos = new List<Pessoa>();
+Curso c1 = new Curso(nome: "Curso de Canto", valor: 1299.99M);
+c1.Alunos = new List<Pessoa>();
 
-//c1.AdicionaAluno(p1);
-//c1.AdicionaAluno(p2);
-//c1.AdicionaAluno(p3);
+c1.AdicionaAluno(p1);
+c1.AdicionaAluno(p2);
+c1.AdicionaAluno(p3);
 
 Console.WriteLine($"O Curso {c1.Nome} possui {c1.ContagemAlunos()} Alunos");
 c1.ListaAlunos();
+*/
